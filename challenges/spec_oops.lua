7701a98f86ae5e75c3794a0d5318aba92d26e21e
threeofaband <sycisytation@gmail.com>
2025-11-21 13:55:33 +0800

    Create spec_oops.lua

---
+++ challenges/spec_oops.lua
@@ -0,0 +1,14 @@
+SMODS.Challenge{
+  key = 'spec_oops',
+  rules = {
+    custom = {
+      {id = 'no_reward'},
+      {id = 'no_interest'},
+    },
+  },
+  jokers = {
+    {id = 'j_roff_spec_oops', eternal = true},
+    {id = 'j_roff_spec_oops', eternal = true},
+    {id = 'j_roff_spec_oops', eternal = true},
+  },
+}

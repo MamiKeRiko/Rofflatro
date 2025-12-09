4f12673bfc836d21c1d4a49daf7ad3e2914c5980
AlrexX <alexsopena789@gmail.com>
2025-12-09 12:47:55 +0100

    just get chips actually works now

--- cards/jokers/Just Get Chips.lua
+++ cards/jokers/Just Get Chips.lua
@@ -3,39 +3,48 @@ SMODS.Joker{
 	atlas = 'roffers',
 	rarity = 3,
 	cost = 6,
-	blueprint_compat = true,
+	blueprint_compat = false,
 	eternal_compat = true,
 	pos = { x = 1, y = 7 },
 	soul_pos = { x = 0, y = 7 },
-	config = { extra = { active = false }},
-	loc_vars = function(self,info_queue,card)
-		return {vars = {}}
-	end,
+	config = { extra = {active = false}},
 	calculate = function(self,card,context)
 
 		if context.setting_blind then
 			card.ability.extra.active = true
+			print("getchips active")
 		end
-		if context.end_of_round then
-			card.ability.extra.active = false -- make sure we're only checking for scoring jokers
+
+		if context.end_of_round and context.cardarea == G.jokers then
+			card.ability.extra.active = false
 		end
-	
+
 		if card.ability.extra.active then
+
+			local effects = {}
+
 			for _, j in ipairs(G.jokers.cards) do
-				if j.ability.name ~= 'Blueprint' and j.ability.name ~= 'Brainstorm' and j.ability.blueprint_compat then -- bp effects create infinite feedback loops if we try copy them...
+				if j ~= card then
+
 					context.blueprint = nil
 					local ret = SMODS.blueprint_effect(card, j, context)
+
 					if ret then
-						-- print(inspect(ret))
-						local chip_effect = ret.chips or ret.chip_mod
+						local chip_effect = (ret.chip_mod > 0 and ret.chip_mod) or ret.chips
 						if chip_effect then
-							return {
-								chips = chip_effect
-							}
+							print(j.ability.name .. " gives " .. chip_effect .. " chips")
+							table.insert(effects, {chips = chip_effect, card = j})
 						end
 					end
 				end
 			end
+
+			return SMODS.merge_effects(effects)
+		end
+	end,
+	add_to_deck = function (self, card, from_debuff)
+		if G.STATE == G.STATES.SELECTING_HAND then -- for judgement
+			card.ability.extra.active = true
 		end
 	end,
 	set_badges = function (self, card, badges)
